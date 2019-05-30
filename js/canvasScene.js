c2de74e07b29f2fc1097434dda72efc7f1ce2855
Wixes <dmitri.koma@yandex.ru>
2019-05-30 15:06:56 +0000

    Add divide option for 1st cube

--- js/canvasScene.js
+++ js/canvasScene.js
@@ -4,8 +4,8 @@ var ambientLight_1, spotlight_1;
 scene = new THREE.Scene();
 scene.background = new THREE.Color({color: 0x00ff00});
 
-camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 5, -5);
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 60, -10);
 camera.lookAt(scene.position);
 
 renderer = new THREE.WebGLRenderer({canvas: artifactCanvas,
@@ -14,13 +14,17 @@ renderer = new THREE.WebGLRenderer({canvas: artifactCanvas,
 
 
 // CUBE
-var cubeGeometry = new THREE.CubeGeometry(3,3,3);
+var cubeGeometry = new THREE.CubeGeometry(50 , 50, 50);
 var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cube.name = "first_cube";
 
 //LIGHT
 spotlight_1 = new THREE.SpotLight(0xffffff);
 ambientLight_1 = new THREE.AmbientLight(0x404040);
+
+// AXES
+var axesHelper = new THREE.AxesHelper(100);
 
 // CONTROLLER
 controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -33,17 +37,18 @@ controls.enableDamping = true;
 controls.dampingFactor = 0.2;
 
 // OBJECT POSITIONS
-spotlight_1.position.set(-3, 5, -3);
+spotlight_1.position.set(-40, 55, -30);
 cube.position.set(0, 0, 0);
 
 // ADD OBJECTS
 scene.add(spotlight_1);
 scene.add(ambientLight_1);
+scene.add(axesHelper);
 scene.add(cube);
 
 scene.updateMatrixWorld(true);
 
-// TEST
+// CHECK MAIN CUBE POSITION
 var pos = new THREE.Vector3();
 pos.setFromMatrixPosition(cube.matrixWorld);
 console.log(pos.x, pos.y, pos.z);
@@ -54,7 +59,7 @@ function animate() {
 
     controls.update();
     //cube.rotation.x += 0.01;
-    cube.rotation.y += 0.02;
+    //cube.rotation.y += 0.02;
 
     renderer.render(scene, camera);
 
--- js/index.js
+++ js/index.js
@@ -19,61 +19,129 @@ function addCube() {
     // VARIABLES
     var allChildren = scene.children;
     var allCubes = 0;
-    var lastObject = allChildren[allChildren.length - 1];
 
-    // CREATE CUBE
-    var cubeSize = Math.ceil((Math.random() * 3));
-    var cubeGeometry = new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize);
-    var cubeMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.name = "cube-" + scene.children.length;
+    var width_1 = 0;
+    var width_2 = 0;
+    var width_3 = 0;
 
-    // TEST
-    if (rand_pos % 5 == 0) {
-        cube.position.x = lastObject.position.x + 4;
-        cube.position.y = lastObject.position.y;
-        cube.position.z = lastObject.position.z;
+    var height_1 = 0;
+    var height_2 = 0;
+    var height_3 = 0;
+
+    var depth_1 = 0;
+    var depth_2 = 0;
+    var depth_3 = 0;
+
+    var position_1 = new THREE.Vector3();
+    var position_2 = new THREE.Vector3();
+    var position_3 = new THREE.Vector3();
+
+    // ALGORHITM FOR CREATING 2 CUBES INSTEAD OF 1
+    // for 1st cube
+    if (allChildren.length == 4 && allChildren[3] instanceof THREE.Mesh) {
+        width_1 = allChildren[3].geometry.parameters.width;
+        height_1 = allChildren[3].geometry.parameters.height;
+        depth_1 = allChildren[3].geometry.parameters.depth;
+        position_1.setFromMatrixPosition(allChildren[2].matrixWorld);
+        console.log ("width: " + width_1 + ", height: " + height_1 + " and depth: " + depth_1);
+        console.log(position_1);
+
+        // CREATE WIDTH, HEIGHT AND DEPTH FOR CUBES
+        var rand_divide = Math.floor((Math.random() * 3) + 1);
+        if (rand_divide == 1) {
+            console.log ("change height");
+            // Create parameters for 2nd cube
+            width_2 = width_1;
+            height_2 = Math.floor((Math.random() * height_1) + 1);
+            depth_2 = depth_1;
+            
+            // Create parameters for 3rd cube
+            width_3 = width_2;
+            height_3 = height_1 - height_2;
+            depth_3 = depth_2;
+
+            // Create position for 2nd cube
+            position_2.x = position_1.x + (width_1 / 2) + (width_2 / 2) + 15;
+            position_2.y = position_1.y + (height_1 / 2) - (height_2 / 2);
+            position_2.z = position_1.z;
+
+            // Create position for 3rd cube
+            position_3.x = position_2.x;
+            position_3.y = position_2.y - (height_2 / 2) - (height_3 / 2) - 3;
+            position_3.z = position_2.z;
+
+        }
+        else if (rand_divide == 2) {
+            console.log ("change width");
+            // Create parameters for 2nd cube
+            width_2 = Math.floor((Math.random() * width_1) + 1);
+            height_2 = height_1;
+            depth_2 = depth_1
+
+            // Create parameters for 3rd cube
+            width_3 = width_1 - width_2;
+            height_3 = height_2;
+            depth_3 = depth_2;
+            
+            // Create position for 2nd cube
+            position_2.x = position_1.x + (width_1 / 2) + (width_2 / 2) + 15;
+            position_2.y = position_1.y;
+            position_2.z = position_1.z;
+
+            // Create position for 3rd cube
+            position_3.x = position_2.x + (width_2 / 2) + (width_3 / 2) + 3;
+            position_3.y = position_2.y;
+            position_3.z = position_2.z;
+        }
+        else {
+            console.log("change depth");
+            // Create parameters for 2nd cube
+            width_2 = width_1;
+            height_2 = height_1;
+            depth_2 = Math.floor((Math.random() * depth_1) + 1);
+
+            // Create parameters for 3rd cube
+            width_3 = width_2;
+            height_3 = height_2;
+            depth_3 = depth_1 - depth_2;
+
+            // Create position for 2nd cube
+            position_2.x = position_1.x + (width_1 / 2) + (width_2 / 2) + 15;
+            position_2.y = position_1.y;
+            position_2.z = position_1.z - (depth_1 / 2) + (depth_2 / 2);
+
+            // Create position for 3rd cube
+            position_3.x = position_2.x;
+            position_3.y = position_1.y;
+            position_3.z = position_2.z + (depth_2 / 2) + (depth_3 / 2) + 3;
+
+        }
+
+        // CREATE NEW CUBES
+        // cube 2
+        var cubeGeometry_2 = new THREE.CubeGeometry(width_2, height_2, depth_2);
+        var cubeMaterial_2 = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
+        var cube_2 = new THREE.Mesh(cubeGeometry_2, cubeMaterial_2);
+        console.log(position_2);
+        cube_2.position.set(position_2.x, position_2.y, position_2.z);
+        scene.add(cube_2);
+
+        // cube 3
+        var cubeGeometry_3 = new THREE.CubeGeometry(width_3, height_3, depth_3);
+        var cubeMaterial_3 = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
+        var cube_3 = new THREE.Mesh(cubeGeometry_3, cubeMaterial_3);
+        console.log(position_3);
+        cube_3.position.set(position_3.x, position_3.y, position_3.z);
+        scene.add(cube_3);
+
+    }
+    // For any of last two created cubes
+    else {
+        width_1 = 100;
+        height_1 = 100;
     }
 
-    if (rand_pos % 5 == 1) {
-        cube.position.x = lastObject.position.x;
-        cube.position.y = lastObject.position.y + 4;
-        cube.position.z = lastObject.position.z;
-    }
-
-    if (rand_pos % 5 == 2) {
-        cube.position.x = lastObject.position.x;
-        cube.position.y = lastObject.position.y;
-        cube.position.z = lastObject.position.z + 4;
-    }
-    if (rand_pos % 5 == 3) {
-        cube.position.x = lastObject.position.x - 4;
-        cube.position.y = lastObject.position.y;
-        cube.position.z = lastObject.position.z;
-    }
-
-    if (rand_pos % 5 == 4) {
-        cube.position.x = lastObject.position.x;
-        cube.position.y = lastObject.position.y - 4;
-        cube.position.z = lastObject.position.z;
-    }
-
-    if (rand_pos % 5 == 5) {
-        cube.position.x = lastObject.position.x;
-        cube.position.y = lastObject.position.y;
-        cube.position.z = lastObject.position.z - 4;
-    }
-
-    rand_pos++;
-
-        // NORMAL
-        //cube.position.x = lastObject.position.x + 4;
-        //cube.position.y = lastObject.position.y;
-        //cube.position.z = lastObject.position.z;
-
-    // ADD CUBE
-    scene.add(cube);
-
+    
     // NUMBER OF OBJECTS
     console.log ("all objects: " + allChildren.length);
     for (var i = 0; i < allChildren.length; i++) {
@@ -86,7 +154,7 @@ function addCube() {
 function removeCube() {
     var allChildren = scene.children;
     var lastObject = allChildren[allChildren.length-1];
-    if (lastObject instanceof THREE.Mesh && allChildren.length > 3) {
+    if (lastObject instanceof THREE.Mesh && allChildren.length > 4) {
         scene.remove(lastObject);
     }
 }
